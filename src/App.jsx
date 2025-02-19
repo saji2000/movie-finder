72a001407452f3435639731ab9404a113fcf8cd5
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-18 21:49:47 -0500

    working on movie card, praise God

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MovieCard from "./components/MovieCard";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <MovieCard movie={{ title: "Sajad's Film", release_date: "2024" }} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  const favoriteMovie = (e) => {
+    alert("clicked");
+  };
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={favoriteMovie}>
+            ❤️
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
