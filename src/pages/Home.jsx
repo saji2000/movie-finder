66affbfa01d4ea12fb667b37c6e477f25b6632bd
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-23 22:35:47 -0500

    useState and search query input, praise God

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 const Home = () => {
-  const searchMovie = () => {};
+  const [searchQuery, setSearchQuery] = useState("");
+  const searchMovie = (e) => {
+    alert(searchQuery);
+    e.preventDefault();
+  };
   const movies = [
     { id: 1, title: "John Wick", release_date: "2017" },
     { id: 2, title: "Terminator", release_date: "1999" },
@@ -15,12 +19,20 @@ const Home = () => {
           type="text"
           placeholder="Search for movies"
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
       <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
       </div>
     </div>
   );
