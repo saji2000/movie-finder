6090abf21ecc1967be29b3f227e96ee6cdc769f4
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-24 23:04:26 -0500

    Nav bar added, praise God

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Favorites from "./pages/Favorites";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 };
 
